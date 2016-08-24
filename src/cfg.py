c547ab4a0ca60b670e01e310ecb8471fecb2a35d
Nicholas.MZ <online-zwl@163.com>
2016-08-24 22:51:40 +0800

    optimize codes of cfg and get the right patch

--- src/cfg.py
+++ src/cfg.py
@@ -1,28 +1,26 @@
 import json
-import os
-from pprint import pprint
-
+import os, inspect
 
 CFG_F_NAME = 'key.json'
 KEY_APPID = 'AppID'
 KEY_APPKEY = 'AppKey'
 KEY_MASTERKEY = 'MasterKey'
 
-key_set = None
 
 def read_key():
 
-    file_path = os.path.join(os.getcwd(), CFG_F_NAME)
+    # file_path = os.path.join(os.getcwd(), CFG_F_NAME)
+    # the os.getcwd return the path of the scrip of the execute
+    dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    print('dir is -->', dir)
+    file_path = os.path.join(dir, CFG_F_NAME)
     print(file_path)
-    with open(CFG_F_NAME) as data_file:
+    with open(file_path) as data_file:
         key = json.load(data_file)
-        id = key[KEY_APPID]
-        appkey = key[KEY_APPKEY]
         mkey = key[KEY_MASTERKEY]
-        # pprint(key)
         print(mkey)
-        key_set = key
     return key
+
 
 def get_app_key():
     return read_key()[KEY_APPKEY]
@@ -33,7 +31,7 @@ def get_app_id():
 
 
 def get_master_key():
-    return key_set[KEY_MASTERKEY]
+    return read_key()[KEY_MASTERKEY]
 
 
 def write_cfg_file():
@@ -45,4 +43,3 @@ def write_cfg_file():
         json.dump(cfg, f)
 
 
-# read_key()
